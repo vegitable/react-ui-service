2f448b89cb1a1821882bb45b388a1d802119f262
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-03-20 16:55:05 +0000

    adds form submit handling for SearchBar

--- src/containers/SearchBar/index.js
+++ src/containers/SearchBar/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './SearchBar.css';
 
-const SearchBar = () => (
-  <div className='searchBar'>
-    Search Bar
-  </div>
-);
+class SearchBar extends React.Component {
+  state = {
+    postcode: ''
+  };
+
+  handleChange = (event) => {
+    this.setState({postcode: event.target.value});
+  };
+
+  handleSubmit = (event) => {
+    alert('A postcode was submitted: ' + this.state.postcode);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className='searchBar'>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            onChange={this.handleChange}
+            placeholder='postcode'
+          ></input>
+          <input type='submit'></input>
+        </form>
+      </div>
+    );
+  }
+};
 
 export default SearchBar;
