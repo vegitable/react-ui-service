7c8d65e132fa8381840d39ed89e0849caf9eb039
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-03-20 15:19:34 +0000

    adds SideBar component

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Header.css';
+import SideBar from '../SideBar';
 
 const Header = () => (
   <div className='header'>
-    Header
+    <SideBar />
   </div>
 );
 
