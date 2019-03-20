fa7a480772fa506b1b26e06de7efca7e8f885d97
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-03-20 15:59:01 +0000

    adds VideoBox component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './containers/Header';
+import VideoBox from './containers/VideoBox';
 import SearchBar from './containers/SearchBar';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <VideoBox />
         <SearchBar />
       </div>
     );
---
+++ src/containers/VideoBox/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideoBox = () => (
+  <div>
+    VideoBox
+  </div>
+);
+
+export default VideoBox;
