b89346fd1ee6ad759f512d003425d40bd72bc75d
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-03-25 09:15:01 +0000

    adds error handling for no data on restaurants route

--- src/containers/RestaurantsPage/index.js
+++ src/containers/RestaurantsPage/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import RestaurantsTileList from '../RestaurantTileList';
+import { withRouter } from 'react-router-dom';
 
 class RestaurantsPage extends React.Component {
+  
+  redirectToHome = () => {
+    this.props.history.push('/');
+  }
+
   render () {
     return (
       <div>
-        <RestaurantsTileList restaurantData={this.props.restaurantData}/> 
+        { this.props.restaurantData.length ? <RestaurantsTileList restaurantData={this.props.restaurantData}/> : this.redirectToHome() }
       </div>
     );
   };
 };
 
-export default RestaurantsPage;
+export default withRouter(RestaurantsPage);
