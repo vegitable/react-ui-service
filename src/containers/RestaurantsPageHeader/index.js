7f9ddd468da4695a26bc18522125bcd6f7e17f64
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-15 18:46:53 +0100

    add distance button

--- src/containers/RestaurantsPageHeader/index.js
+++ src/containers/RestaurantsPageHeader/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { faUtensils, faStar } from "@fortawesome/free-solid-svg-icons";
+import { faUtensils, faStar, faRoute } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './RestaurantsPageHeader.css';
 
 const RestaurantsPageHeader = () => (
   <div className='restaurantsPageHeader'>
+    <div>
+      <FontAwesomeIcon icon={faRoute} />
+    </div>
     <div>
       <FontAwesomeIcon icon={faUtensils} />
     </div>
