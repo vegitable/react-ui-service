1c3398c37bb4d1eb5a2064aa3ad02148178fded1
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-17 16:38:52 +0100

    adds selected button style function

--- src/containers/RestaurantsPageHeader/index.js
+++ src/containers/RestaurantsPageHeader/index.js
@@ -4,19 +4,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './RestaurantsPageHeader.css';
 
 const RestaurantsPageHeader = (props) => {
+  const unselectedButtonStyle = () => {
+    return {
+      color: '#E2C2C6',
+      backgroundColor: '#fff'
+    };
+  };
+
+  const selectedButtonStyle = () => {
+    return {
+      color: '#610F7F',
+      backgroundColor: 'none'
+    };
+  };
+
+  const buttonStyleArray = {
+    distanceStyle: selectedButtonStyle(), 
+    dishesStyle: unselectedButtonStyle(), 
+    ratingStyle: unselectedButtonStyle()
+  };
+
   const orderByButtonClicked = (button) => {
     props.changeDisplayOrder(button);
   };
 
   return (
     <div className='restaurantsPageHeader'>
-      <div onClick={() => orderByButtonClicked('distance')}>
+      <div onClick={() => orderByButtonClicked('distance')} style={buttonStyleArray.distanceStyle}>
         <FontAwesomeIcon icon={faRoute} />
       </div>
-      <div onClick={() => orderByButtonClicked('dishes')}>
+      <div onClick={() => orderByButtonClicked('dishes')} style={buttonStyleArray.dishesStyle}>
         <FontAwesomeIcon icon={faUtensils} />
       </div>
-      <div onClick={() => orderByButtonClicked('rating')}>
+      <div onClick={() => orderByButtonClicked('rating')} style={buttonStyleArray.ratingStyle}>
         <FontAwesomeIcon icon={faStar} />
       </div>
     </div>
