7d10219245c8a221d356db1b3d10fe4d853cbeda
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-18 08:57:52 +0100

    converts RestaurantsPageHeader to class

--- src/containers/RestaurantsPageHeader/index.js
+++ src/containers/RestaurantsPageHeader/index.js
@@ -1,49 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { faUtensils, faStar, faRoute } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './RestaurantsPageHeader.css';
 
-const RestaurantsPageHeader = (props) => {
-  const unselectedButtonStyle = () => {
+class RestaurantsPageHeader extends Component {
+  unselectedButtonStyle = () => {
     return {
       color: '#E2C2C6',
       backgroundColor: '#fff'
     };
   };
 
-  const selectedButtonStyle = () => {
+  selectedButtonStyle = () => {
     return {
       color: '#610F7F',
       backgroundColor: 'none'
     };
   };
 
-  const buttonStyles = {
-    distance: selectedButtonStyle(), 
-    dishes: unselectedButtonStyle(), 
-    rating: unselectedButtonStyle()
+  buttonStyles = {
+    distance: this.selectedButtonStyle(), 
+    dishes: this.unselectedButtonStyle(), 
+    rating: this.unselectedButtonStyle()
   };
 
-  const orderByButtonClicked = (button) => {
-    props.changeDisplayOrder(button);
-    for (var style in buttonStyles) {
-      console.log(style);
-    }
+  orderByButtonClicked = (button) => {
+    for (var style in this.buttonStyles) {
+      if (button === style) {
+        this.buttonStyles[style] = this.selectedButtonStyle();
+        console.log(this.buttonStyles[style])
+      } else {
+        this.buttonStyles[style] = this.unselectedButtonStyle();
+        console.log(this.buttonStyles[style])
+      };
+    };
+    this.props.changeDisplayOrder(button);
   };
 
-  return (
-    <div className='restaurantsPageHeader'>
-      <div onClick={() => orderByButtonClicked('distance')} style={buttonStyles.distance}>
-        <FontAwesomeIcon icon={faRoute} />
+  render() {
+    return (
+      <div className='restaurantsPageHeader'>
+        <div onClick={() => this.orderByButtonClicked('distance')} style={this.buttonStyles.distance}>
+          <FontAwesomeIcon icon={faRoute} />
+        </div>
+        <div onClick={() => this.orderByButtonClicked('dishes')} style={this.buttonStyles.dishes}>
+          <FontAwesomeIcon icon={faUtensils} />
+        </div>
+        <div onClick={() => this.orderByButtonClicked('rating')} style={this.buttonStyles.rating}>
+          <FontAwesomeIcon icon={faStar} />
+        </div>
       </div>
-      <div onClick={() => orderByButtonClicked('dishes')} style={buttonStyles.dishes}>
-        <FontAwesomeIcon icon={faUtensils} />
-      </div>
-      <div onClick={() => orderByButtonClicked('rating')} style={buttonStyles.rating}>
-        <FontAwesomeIcon icon={faStar} />
-      </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default RestaurantsPageHeader;
